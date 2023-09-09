6ef40ce5b4f5b367f4d9ec01df3c020bf3f6782b
MahalakshmiS97 <mahalakshmivarma97@gmail.com>
2023-09-10 02:25:25 +0530

    half completed

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-let images = ["dice-1.svg",
-"dice-2.svg",
-"dice-3.svg",
-"dice-4.svg",
-"dice-5_1.svg",
-"dice-6.svg"];
+let images = ["dice-1.PNG","dice-1.PNG",
+"dice-2.PNG",
+"dice-3.PNG",
+"dice-4.PNG",
+"dice-5.PNG",
+"dice-6.PNG"];
+var playing=true;
+var currentscore, sum=0, sum1=0, currentscore1;
 let dice=document.querySelectorAll("img");
 function roll(){
-   let dieOneValue = Math.floor(Math.random()*6 + 1);
-   console.log(dieOneValue);
-   document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+   let p1value = Math.floor(Math.random()*6+1);
+   console.log(p1value);
+   document.querySelector("#die-1").setAttribute("src", images[p1value]);
+   currentscore=p1value;
+   if(sum>=30){
+    document.querySelector("#result").innerHTML="Player 1 Wins";
+ }
+ else{
+   sum = sum + currentscore;
+   document.querySelector("#p1total").innerHTML=sum;
+ }
 }
+
+function roll1(){
+    let p2value = Math.floor(Math.random()*6);
+    console.log(p2value);
+    document.querySelector("#die-1").setAttribute("src", images[p2value]);
+    currentscore1=p2value;
+    if(sum1>=30){
+        document.querySelector("#result").innerHTML="Player 2 Wins";
+    }
+    else{
+   sum1 = sum1 + currentscore1;
+   document.querySelector("#p2total").innerHTML=sum1;
+    }
+ }
+ function reset(){
+    currentscore=0;
+    currentscore1=0;
+    document.querySelector("#p1total").innerHTML=currentscore;
+    document.querySelector("#p2total").innerHTML=currentscore1;
+ }
